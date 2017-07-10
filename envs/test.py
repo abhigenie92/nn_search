0c95be4e7eb17bddbd39587a4389da81e3d5f94a
Abhishek Bhatia <abhigenie92@gmail.com>
2017-07-10 12:58:12 -0400

    working test case for random selection

---
+++ envs/test.py
@@ -0,0 +1,33 @@
+import unittest
+
+import gym,ipdb
+from nn_env import NnEnv
+from time import sleep
+
+class TestLeftRightEnv(unittest.TestCase):
+
+    def test_env(self):
+        env = NnEnv()
+        env.reset()
+        done=False
+        while not done:
+            #env.render()
+            action = env.action_space.sample()
+            observation, reward, done, info = env.step(action)
+            sleep(0.05)
+
+        return True
+
+def test_debug():
+	env = NnEnv()
+	env.reset()
+	done=False
+	while not done:
+	    #env.render()
+	    action = env.action_space.sample()
+	    #print action
+	    observation, reward, done, info = env.step(action)
+	    print "Env",observation, reward, done
+
+if __name__ == '__main__':
+	test_debug()
